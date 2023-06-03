a6f5b24ca9360b8f1cb74675dc9ab4b4e84c602f
Phixyn <phixyn@gmail.com>
2023-06-03 21:49:51 +0100

    Implement languages controller
    Allows HTTP GET requests to retrieve languages from the DB.
    Also able to get language by Guid.

---
+++ Controllers/LanguagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace LexicontactAPI.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        private readonly ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        // GET api/languages
+        public IEnumerable<Language> Get()
+        {
+            return _ctx.Language.ToList();
+        }
+
+        // GET api/languages/076BE845-86F0-43C8-A50F-07A038FD2108
+        public Language Get(string id)
+        {
+            Guid languageGuid = new Guid(id);
+            // TODO Error handling?
+            Language result = _ctx.Language.Find(languageGuid);
+            return result;
+        }
+    }
+}
---
+++ Models/Language.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace LexicontactAPI
+{
+    [Table("language")]
+    public class Language
+    {
+        [Key]
+        public Guid Guid { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
